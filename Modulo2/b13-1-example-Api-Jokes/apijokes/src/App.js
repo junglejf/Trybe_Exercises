acec593121029ddfeb974c733fcefb453a272704
junglejf <jfcchagas@id.uff.br>
2021-08-05 17:16:09 -0300

    redenderizaoao piada

---
+++ Modulo2/b13-1-example-Api-Jokes/apijokes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DadJoke from './Components/DadJoke';
+
+function App() {
+  return (
+    <div className="App">
+      <DadJoke />
+    </div>
+  );
+}
+
+export default App;
